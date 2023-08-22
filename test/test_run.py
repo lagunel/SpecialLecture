485756c2f7684126518bd35c76641f3ba7c6ee2d
ura <a601021043@st.tachibana-u.ac.jp>
2023-08-22 13:46:08 +0900

    sonar9

--- test/test_run.py
+++ test/test_run.py
@@ -4,19 +4,19 @@ from speciallecture.CSVPrinter import CSVPrinter
 class TestCSVPrinter(unittest.TestCase):
 
     def test_read(self):
-        printer = CSVPrinter("sample.csv")
+        printer = CSVPrinter("test/sample.csv")
         l = printer.read()
         self.assertEqual(3, len(l))
 
     def test_a(self):
-        printer = CSVPrinter("sample.csv")
+        printer = CSVPrinter("test/sample.csv")
         l = printer.read()
         print(l)
         self.assertEqual(3, len(l))
 
 
     def test_b(self):
-        printer = CSVPrinter("sample.csv")
+        printer = CSVPrinter("test/sample.csv")
         l = printer.read()
         self.assertEqual("b2", l[1][1])
 
